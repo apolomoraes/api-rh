d748b73ba670c2dd6402cfee01d44e1a6610ab4b
Apolo Moraes <vlogsapolo@gmail.com>
2023-07-24 23:36:05 -0300

    added methods create, update, show to funcionarios

--- src/main/java/com/githubapolomoraes/rh/controller/FuncionarioController.java
+++ src/main/java/com/githubapolomoraes/rh/controller/FuncionarioController.java
@@ -1,20 +1,50 @@
 package com.githubapolomoraes.rh.controller;
 
 import com.githubapolomoraes.rh.model.Funcionario;
+import com.githubapolomoraes.rh.repository.FuncionarioRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 
+    private final FuncionarioRepository funcionarioRepository;
+
+    public FuncionarioController(FuncionarioRepository funcionarioRepository) {
+        this.funcionarioRepository = funcionarioRepository;
+    }
+
     @GetMapping
-    public String testar() {
-        return "testanto123...";
+    public List<Funcionario> listar() {
+        return funcionarioRepository.findAll();
+    }
+
+    @GetMapping("/{funcionarioId}")
+    public ResponseEntity<Funcionario> buscar(@PathVariable Long funcionarioId) {
+        return funcionarioRepository.findById(funcionarioId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Funcionario cadastrar(@RequestBody Funcionario funcionario) {
-        System.out.println(funcionario);
-        return funcionario;
+        return funcionarioRepository.save(funcionario);
+    }
+
+    @PutMapping("/{funcionarioId}")
+    public ResponseEntity<Funcionario> atualizar(@PathVariable Long funcionarioId, @RequestBody Funcionario funcionario) {
+        if (!funcionarioRepository.existsById(funcionarioId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        funcionario.setId(funcionarioId);
+        Funcionario funcionarioAtualizado = funcionarioRepository.save(funcionario);
+
+        return ResponseEntity.ok(funcionarioAtualizado);
     }
 }
--- src/main/java/com/githubapolomoraes/rh/repository/FuncionarioRepository.java
+++ src/main/java/com/githubapolomoraes/rh/repository/FuncionarioRepository.java
@@ -1,5 +1,10 @@
 package com.githubapolomoraes.rh.repository;
 
-public interface FuncionarioRepository {
+import com.githubapolomoraes.rh.model.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
 
 }
