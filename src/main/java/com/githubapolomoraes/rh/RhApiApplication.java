3a76a6eccb87ee96a283833b999f33b5c89733bc
Apolo Moraes <vlogsapolo@gmail.com>
2023-07-24 22:28:01 -0300

    first commit

---
+++ src/main/java/com/githubapolomoraes/rh/RhApiApplication.java
@@ -0,0 +1,13 @@
+package com.githubapolomoraes.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhApiApplication.class, args);
+	}
+
+}
